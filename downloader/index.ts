d00ce55347970bd00d2b88b2a591a50c18b81b1f
Marcos Dias <215209615+marcoscdias@users.noreply.github.com>
2025-11-19 19:32:49 -0300

    chore: Implement review fetching

--- downloader/index.ts
+++ downloader/index.ts
@@ -1,8 +1,58 @@
 import { Octokit } from "octokit";
-import { delay, Listr, type ListrTask, type ListrTaskWrapper, PRESET_TIMER, Spinner } from "listr2";
+import { throttling } from "@octokit/plugin-throttling";
+import {
+  Listr,
+  ListrLogger,
+  ListrLogLevels,
+  type ListrTask,
+  type ListrTaskWrapper,
+  PRESET_TIMER,
+  Spinner
+} from "listr2";
 
-const octokit = new Octokit({
-  auth: process.env.GH_TOKEN
+const logger = new ListrLogger({ useIcons: true })
+
+const MyOctokit = Octokit.plugin(throttling);
+const octokit = new MyOctokit({
+  auth: process.env.GH_TOKEN,
+  log: {
+    debug: message => logger.log(ListrLogLevels.FAILED, message),
+    info: message => logger.log(ListrLogLevels.FAILED, message),
+    warn: message => logger.log(ListrLogLevels.FAILED, message),
+    error: message => logger.log(ListrLogLevels.FAILED, message),
+  },
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit, retryCount) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      );
+
+      if (retryCount < 50) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      } else {
+        octokit.log.error(
+          `Failed after too many attempts!`,
+        );
+      }
+    },
+    onSecondaryRateLimit: (retryAfter, options, octokit, retryCount) => {
+      octokit.log.warn(
+        `Secondary rate limit detected for request ${options.method} ${options.url}`,
+      );
+
+      if (retryCount < 10) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      } else {
+        octokit.log.error(
+          `Failed after too many attempts!`,
+        );
+      }
+    },
+  },
 });
 
 const repoPath = Bun.argv[2];
@@ -31,26 +81,79 @@ function time(name: string, promise: (task: ListrTaskWrapper<Ctx, any, any>) => 
 }
 
 async function fetchData(task: ListrTaskWrapper<Ctx, any, any>, api: string, repo: Repository) {
+  const file = Bun.file(`downloads/${repoPath}/${api.replaceAll("/", "_")}.json`);
+  if (await file.exists()) {
+    return task;
+  }
+
   return task.newListr([
       {
         title: "Fetching data",
         task: async ctx => {
           ctx[api] = await octokit.paginate(`GET /repos/{owner}/{repo}/${api}`, {
-            owner: repoParts[0],
-            repo: repoParts[1],
+            owner: repo.owner,
+            repo: repo.repo,
             per_page: 100,
             state: "all",
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
+            headers: {'X-GitHub-Api-Version': '2022-11-28'}
           });
         },
       },
       {
         title: "Saving data",
         task: async (ctx, subTask) => {
-          const file = Bun.file(`downloads/${repoPath}/${api.replaceAll("/", "_")}.json`);
           await file.write(JSON.stringify(ctx[api]));
+        }
+      }
+    ],
+    {
+      concurrent: false,
+    }
+  );
+}
+
+async function fetchReview(task: ListrTaskWrapper<Ctx, any, any>, repo: Repository) {
+  const file = Bun.file(`downloads/${repoPath}/pulls_reviews.json`);
+  const pullsFile = Bun.file(`downloads/${repoPath}/pulls.json`);
+  if (await file.exists()) {
+    return task;
+  }
+
+  const pulls: any[] = JSON.parse(await pullsFile.text());
+  const reviews: Record<string, any[]> = {};
+
+  const batchSize = 16;
+
+  return task.newListr([
+      {
+        title: "Fetching reviews",
+        task: async (ctx, subTask) => {
+          for (let i = 0; i < pulls.length; i += batchSize) {
+            const batch = pulls.slice(i, i + batchSize);
+
+            subTask.output = "yipyipyip"
+            subTask.title = `Fetching reviews (${i + 1}-${Math.min(i + batchSize, pulls.length)} of ${pulls.length})`;
+
+            await Promise.all(
+              batch.map(async pull => {
+                reviews[`${pull.number}`] = await octokit.paginate("GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews", {
+                    owner: repoParts[0],
+                    repo: repoParts[1],
+                    pull_number: pull.number,
+                    per_page: 100,
+                    headers: {'X-GitHub-Api-Version': '2022-11-28'},
+                  }
+                );
+              })
+            );
+          }
+          ctx["pulls/reviews"] = reviews;
+        }
+      },
+      {
+        title: "Saving data",
+        task: async (ctx, subTask) => {
+          await file.write(JSON.stringify(ctx["pulls/reviews"]));
         }
       }
     ],
@@ -66,7 +169,7 @@ const repo = {
 }
 
 type Ctx = {
-  [id: string]: unknown[]
+  [id: string]: any
 }
 
 class NewSpinner extends Spinner {
@@ -100,7 +203,8 @@ tasks.add([
   time("issues", task => fetchData(task, "issues", repo)),
   time("issue comments", task => fetchData(task, "issues/comments", repo)),
   time("pull requests", task => fetchData(task, "pulls", repo)),
-  time("PR review comments", task => fetchData(task, "pulls/comments", repo))
+  time("PR review comments", task => fetchData(task, "pulls/comments", repo)),
+  time("PR reviews", task => fetchReview(task, repo)),
 ]);
 
 await tasks.run();
